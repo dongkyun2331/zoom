45c9d7004423b36ec6f028e4cfd104623da700fc
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-19 21:35:24 +0900

    a

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => res.render("home"));
 
