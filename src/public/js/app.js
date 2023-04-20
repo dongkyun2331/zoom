d56a13a71475368c0e4b1c31b4a9ebbdb51c1725
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-20 18:16:05 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,5 +9,5 @@ socket.addEventListener("message", (message) => {
 });
 
 socket.addEventListener("close", () => {
-  console.log("Connected from Server ❌");
+  console.log("Disconnected from Server ❌");
 });
