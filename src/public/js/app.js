3a07d36b4b556cddf88cec6d264aa0126d29b2a3
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-20 18:11:52 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,13 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
-  console.log("Connected to Server ✔");
+  console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
   console.log("Just got this: ", message, " form the Server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Connected from Server ❌");
 });
