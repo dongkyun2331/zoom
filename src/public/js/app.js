c7e90f88203465db0d3cf7f2d733449d7cab157f
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-21 21:04:51 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,11 +6,9 @@ const form = welcome.querySelector("form");
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit(
-    "enter_room",
-    { payload: input.value },
-    console.log("server is done!")
-  );
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
   input.value = "";
 }
 
