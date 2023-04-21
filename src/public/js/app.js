9109cba0dd754aa55f89cc3c2c0774da143aea32
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-21 14:48:28 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -34,10 +34,7 @@ function handleSubmit(event) {
 function handleNickSubmit(event) {
   event.preventDefault();
   const input = nickForm.querySelector("input");
-  socket.send({
-    type: "nickname",
-    payload: input.value,
-  });
+  socket.send(makeMessage("nickname", input.value));
 }
 
 messageForm.addEventListener("submit", handleSubmit);
