de0a50dfd364c3d4aca0bb981d943d64295b73cf
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-21 11:24:56 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,7 +15,3 @@ socket.addEventListener("message", (message) => {
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
-
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
