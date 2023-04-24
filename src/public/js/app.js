1adc406d0891acbbf5c9c6cc9c9c4702bc7de279
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-24 15:08:39 +0900

    a

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,15 +6,19 @@ const room = document.getElementById("room");
 
 room.hidden = true;
 
+let roomName;
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
+  const h3 = room.querySelector("h3");
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
   socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
