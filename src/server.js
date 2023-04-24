362c4dedb0243de519b69694aefc35a5bb6121b0
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-24 19:38:01 +0900

    ㅁ

--- src/server.js
+++ src/server.js
@@ -22,6 +22,9 @@ wsServer.on("connection", (socket) => {
     done();
     socket.to(roomName).emit("welcome");
   });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
 });
 
 // const wss = new WebSocket.Server({ server });
