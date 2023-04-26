3e86bff290bb08d0e6819385be33c76aefa3405d
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-26 14:48:51 +0900

    a

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ function publicRooms() {
 }
 
 function countRoom(roomName) {
-  return wsServer.socket.adapter.rooms.get(roomName)?.size;
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 }
 
 wsServer.on("connection", (socket) => {
