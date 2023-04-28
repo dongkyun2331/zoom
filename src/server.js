de1169c768edf369679772d6b74a7916ecf40e82
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-28 15:15:16 +0900

    a

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -11,7 +11,7 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer);
+const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
   socket.on("join_room", (roomName) => {
